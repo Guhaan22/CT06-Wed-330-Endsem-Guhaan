88f925e59086e340892d395cb05e29abce1de9e3
Guhaan22 <lakshmi.rbala@gmail.com>
2025-10-22 08:08:00 +0000

    Oct 22, 2025, 8:08 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,13 @@
+let posX, xSpeed;
+function setup(){
+    createCanvas(600, 400);
+    posX = width/ 2;
+    xSpeed = 5;
+    fill("skyblue");
+    background("skyblue")
+}
+function draw(){
+    circle(300,200,50); 
+}
+function moveCircle()
 
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,12 @@
+let pokemons = ["Pikachu","Raichu","Bulbasaur","Mewtwo","Charizard"];
+let yPos;
 
+function setup(){
+    createCanvas(200, 300);
+    fill("pink");
+    // Task: Align the text to the center using textAlign
+    textAlign(300,300)
+    // Task: set font size to 20 using textSize
+    yPos = 100;
+    textSize(20)
+}
