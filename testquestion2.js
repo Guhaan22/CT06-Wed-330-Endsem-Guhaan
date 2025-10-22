ed157d10addde11d0c5b2952a3af43d802616eea
Guhaan22 <lakshmi.rbala@gmail.com>
2025-10-22 08:15:38 +0000

    Oct 22, 2025, 8:15 AM

--- testquestion2.js
+++ testquestion2.js
@@ -5,7 +5,7 @@ function setup(){
     createCanvas(200, 300);
     fill("pink");
     // Task: Align the text to the center using textAlign
-    textAlign(300,300)
+    textAlign()
     // Task: set font size to 20 using textSize
     yPos = 100;
     textSize(20)
